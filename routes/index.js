4eb65fa5f283cff962dbb5528ecde37e68912c21
DESKTOP-AVE7EKK\codeplay <guojunyan93@qq.com>
2016-09-15 18:11:19 +0800

    add many articles; add a login-button and a reg=button on the right-side of the top; basic function is now completed

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var usr=require('dao/dbConnect');
 /* GET home page. */
 
 router.get('/', function(req, res) {
+	var login_num = req.query.login_num;
+	console.log("login_num is :"+login_num);
     if(req.cookies.islogin){                                                                           
         req.session.islogin=req.cookies.islogin;
     }
@@ -13,6 +15,7 @@ router.get('/', function(req, res) {
   res.render('index', { 
 	title: '首页', 
 	test:res.locals.islogin,
+	login_num:login_num,
 	status1: 'active',status2: '',status3: '',status4: '',
 	status5: '',status6: '',status7: '',status8: '',status9: ''
 	});
@@ -125,6 +128,45 @@ router.get('/news', function (req, res, next) {
         error: req.flash('error').toString() */
     });
 });
+router.get('/news02', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('news02', {
+        title: '政策咨询',
+		status1: '',status2: '',status3: 'active',status4: '',status5: '',status6: '',status7: '',status8: '',status9: '',
+		test:res.locals.islogin,
+    });
+});
+router.get('/news03', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('news03', {
+        title: '学术论文',
+		status1: '',status2: '',status3: 'active',status4: '',status5: '',status6: '',status7: '',status8: '',status9: '',
+		test:res.locals.islogin,
+    });
+});
+router.get('/news04', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('news04', {
+        title: '慢病咨询',
+		status1: '',status2: '',status3: 'active',status4: '',status5: '',status6: '',status7: '',status8: '',status9: '',
+		test:res.locals.islogin,
+    });
+});
 router.get('/news_details', function (req, res, next) {
 	if(req.session.islogin){
         res.locals.islogin=req.session.islogin;
@@ -170,6 +212,94 @@ router.get('/research', function (req, res, next) {
         test:res.locals.islogin,
     });
 });
+router.get('/research01', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('research01', {
+        title: '心脑血管病研究中心',
+		status1: '',status2: '',status3: '',status4: 'active',status5: '',status6: '',status7: '',status8: '',status9: '',
+        test:res.locals.islogin,
+    });
+});
+router.get('/research02', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('research02', {
+        title: '肿瘤研究中心',
+		status1: '',status2: '',status3: '',status4: 'active',status5: '',status6: '',status7: '',status8: '',status9: '',
+        test:res.locals.islogin,
+    });
+});
+router.get('/research03', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('research03', {
+        title: '慢阻病研究中心',
+		status1: '',status2: '',status3: '',status4: 'active',status5: '',status6: '',status7: '',status8: '',status9: '',
+        test:res.locals.islogin,
+    });
+});
+router.get('/research04', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('research04', {
+        title: '医养结合研究中心',
+		status1: '',status2: '',status3: '',status4: 'active',status5: '',status6: '',status7: '',status8: '',status9: '',
+        test:res.locals.islogin,
+    });
+});
+router.get('/research_details', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+	//selectRes
+	var id = req.query.id;
+	client=usr.connect();
+	result = null;
+	usr.selectRes(client,id, function (result) {
+		//console.log("the doctor is:"+result[0].name);
+		res.render('research_details', {
+			title: result[0].title,
+			status1: '',status2: '',status3: '',status4: 'active',status5: '',status6: '',status7: '',status8: '',status9: '',
+			test:res.locals.islogin,
+			research:result[0],
+		});
+		
+	});
+});
+//研究基地
+router.get('/bases', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+    res.render('bases', {
+        title: '心脑血管病研究中心',
+		status1: '',status2: '',status3: '',status4: '',status5: 'active',status6: '',status7: '',status8: '',status9: '',
+        test:res.locals.islogin,
+    });
+});
 //专家资源
 router.get('/doctor_more', function (req, res, next) {
 	if(req.session.islogin){
@@ -180,8 +310,30 @@ router.get('/doctor_more', function (req, res, next) {
     }
     res.render('doctor_more', {
         title: '专家资源',
-		status1: '',status2: '',status3: '',status4: '',status5: 'active',status6: '',status7: '',status8: '',status9: '',
+		status1: '',status2: '',status3: '',status4: '',status5: '',status6: 'active',status7: '',status8: '',status9: '',
         test:res.locals.islogin,
     });
+});
+router.get('/doctor_info', function (req, res, next) {
+	if(req.session.islogin){
+        res.locals.islogin=req.session.islogin;
+    }
+    if(req.cookies.islogin){
+        req.session.islogin=req.cookies.islogin;
+    }
+	//selectDocs
+	var id = req.query.id;
+	client=usr.connect();
+	result = null;
+	usr.selectDocs(client,id, function (result) {
+		console.log("the doctor is:"+result[0].name);
+		res.render('doctor_info', {
+			title: '专家详情',
+			status1: '',status2: '',status3: '',status4: '',status5: '',status6: 'active',status7: '',status8: '',status9: '',
+			test:res.locals.islogin,
+			doctor:result[0],
+		});
+		
+	});
 });
 module.exports = router;
