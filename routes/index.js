e965c8f6e9e0f53b17092238c278643c18224574
郭俊彦-acer <guojunyan93@qq.com>
2016-10-20 16:16:30 +0800

    引入文件上传模块，更新数据库

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var usr=require('dao/dbConnect');
+var formidable = require('formidable'),
+	  fs = require('fs'),
+	AVATAR_UPLOAD_FOLDER = '/avatar/'
 /* GET home page. */
+router.get('/upload', function(req, res) {
+  res.render('upload', { title: "文件上传to_GUO's_PC" });
+});
+
+router.post('/upload', function(req, res) {
+
+  var form = new formidable.IncomingForm();   //创建上传表单
+	  form.encoding = 'utf-8';		//设置编辑
+	  form.uploadDir = 'public' + AVATAR_UPLOAD_FOLDER;	 //设置上传目录
+	  form.keepExtensions = true;	 //保留后缀
+	  form.maxFieldsSize = 2 * 1024 * 1024* 1024;   //文件大小
+
+	form.parse(req, function(err, fields, files) {
+
+		if (err) {
+		  res.locals.error = err;
+		  res.render('upload', { title: "上传失败" });
+		  return;		
+		}  
+	   
+		var extName = '';  //后缀名
+		switch (files.fulAvatar.type) {
+			case 'image/pjpeg':
+				extName = 'jpg';
+				break;
+			case 'image/jpeg':
+				extName = 'jpg';
+				break;		 
+			case 'image/png':
+				extName = 'png';
+				break;
+			case 'image/x-png':
+				extName = 'png';
+				break;		 
+		}
+
+		if(extName.length == 0){
+			  res.locals.error = '只支持png和jpg格式图片';
+			  res.render('upload', { title: "上传失败" });
+			  return;				   
+		}
+
+		var avatarName = Math.random() + '.' + extName;
+		var newPath = form.uploadDir + avatarName;
+
+		console.log(newPath);
+		fs.renameSync(files.fulAvatar.path, newPath);  //重命名
+	});
+
+	res.locals.success = '上传成功';
+	res.render('upload', { title: "文件上传成功" });	  
+});
+
 
 router.get('/', function(req, res) {
 	var login_num = 0;
